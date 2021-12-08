f3622ac2cc6e897fc592ab881c9c67acc0117c6e
Hybrid The Akuma <tyhbrid@gmail.com>
2021-12-08 14:11:59 -0600

    better

--- SimpleSaveUtility/Runtime/SaveManager.cs
+++ SimpleSaveUtility/Runtime/SaveManager.cs
@@ -1,4 +1,5 @@
-using System;
+using Codice.CM.WorkspaceServer;
+using UnityEngine;
 
 namespace SimpleSaveUtility
 {
@@ -21,9 +22,103 @@ namespace SimpleSaveUtility
       }
 
       //----------------------------------------------------------------------------------------------------
-      internal static void LoadDataInto(Data data)
+      internal static void LoadDataInto<T>(Data<T> data)
       {
-         throw new NotImplementedException();
+         throw new System.NotImplementedException();
+      }
+
+      //----------------------------------------------------------------------------------------------------
+      internal static void SaveDataFrom<T>(Data<T> data)
+      {
+         throw new System.NotImplementedException();
+      }
+
+      //-//----------------------------------------------------------------------------------------------------
+      internal class Data<T>
+      {
+         internal bool loaded = false;
+         internal string key;
+         internal string savedValue;
+         private T value;
+         internal T defaultValue;
+
+         //-||-------------------------------------------------------------------------------------------------
+         public Data(string key, T defaultValue)
+         {
+            this.key = key;
+            this.value = this.defaultValue = defaultValue;
+         }
+
+         public T Value
+         {
+            get
+            {
+               if (!loaded)
+               {
+                  SaveManager.LoadDataInto(this);
+                  Deserialize(savedValue);
+               }
+               return value;
+            }
+            set
+            {
+               this.value = value;
+               Serialize(value);
+               SaveManager.SaveDataFrom(this);
+            }
+         }
+
+         //----------------------------------------------------------------------------------------------------
+         private void Deserialize(string value)
+         {
+            if (this.value is ISaveable s && s != null)
+            {
+               ((ISaveable)this.value).Deserialize(value);
+               return;
+            }
+
+            object newValue = null;
+            switch (this.value)
+            {
+               default:
+                  Debug.LogError($"Cannot Deserialize type: \"{typeof(T)}\". It must implement \"{nameof(ISaveable)}\", or be a default type.");
+                  break;
+
+               case int:
+                  int.TryParse(value, out var i);
+                  newValue = i;
+                  break;
+
+               case double:
+                  double.TryParse(value, out var d);
+                  newValue = d;
+                  break;
+
+               case string:
+                  newValue = value;
+                  break;
+            }
+
+            if (newValue != null)
+               this.value = (T)newValue;
+         }
+
+         //----------------------------------------------------------------------------------------------------
+         private void Serialize(T value)
+         {
+            if (this.value is ISaveable s && s != null)
+            {
+               savedValue = s.Serialize();
+               return;
+            }
+
+            savedValue = value.ToString();
+         }
+
+         public override string ToString()
+         {
+            return $"Key: \"{key}\", Value: \"{value}\"";
+         }
       }
    }
 
@@ -36,114 +131,59 @@ namespace SimpleSaveUtility
    }
 
    //-//----------------------------------------------------------------------------------------------------
-   internal class Data
-   {
-      internal bool loaded = false;
-      internal string key;
-      internal string savedValue;
-   }
-
-   //-//----------------------------------------------------------------------------------------------------
    public class SavedString
    {
-      private readonly Data data;
+      private readonly SaveManager.Data<string> data;
 
-      public string Value
-      {
-         get
-         {
-            if (!data.loaded)
-               SaveManager.LoadDataInto(data);
-            return data.savedValue;
-         }
-      }
+      public string Value => data.Value;
 
       //-||-------------------------------------------------------------------------------------------------
       public SavedString(string key, string defaultValue)
       {
-         data = new Data();
-         data.key = key;
-         data.savedValue = defaultValue;
+         data = new SaveManager.Data<string>(key, defaultValue);
       }
 
       public override string ToString()
       {
-         return $"Key: \"{data.key}\", Value: \"{data.savedValue}\"";
+         return data.ToString();
       }
    }
 
    //-//----------------------------------------------------------------------------------------------------
    public class SavedDouble
    {
-      private readonly Data data;
-      private double value;
+      private readonly SaveManager.Data<double> data;
 
-      public double Value
-      {
-         get
-         {
-            if (!data.loaded)
-            {
-               SaveManager.LoadDataInto(data);
-               value = Deserialize(data.savedValue);
-            }
-
-            return value;
-         }
-      }
+      public double Value => data.Value;
 
       //-||-------------------------------------------------------------------------------------------------
       public SavedDouble(string key, double defaultValue)
       {
-         data = new Data();
-         data.key = key;
-         data.savedValue = Serialize(defaultValue);
-      }
-
-      //----------------------------------------------------------------------------------------------------
-      private double Deserialize(string value)
-      {
-         double.TryParse(value, out double result);
-         return result;
-      }
-
-      //----------------------------------------------------------------------------------------------------
-      private string Serialize(double value)
-      {
-         return value.ToString();
+         data = new SaveManager.Data<double>(key, defaultValue);
       }
 
       public override string ToString()
       {
-         return $"Key: \"{data.key}\", Value: \"{data.savedValue}\"";
+         return data.ToString();
       }
    }
 
    //-||----------------------------------------------------------------------------------------------------
    public class SavedData<T> where T : ISaveable
    {
-      private readonly Data data;
-      private T value;
+      private readonly SaveManager.Data<T> data;
 
-      public T Value
-      {
-         get
-         {
-            if (!data.loaded)
-            {
-               SaveManager.LoadDataInto(data);
-               value.Deserialize(data.savedValue);
-            }
-            return value;
-         }
-      }
+      public T Value => data.Value;
 
       //-||-------------------------------------------------------------------------------------------------
-      public SavedData(string key, T defaultValue = default)
+      public SavedData(string key, T defaultValue)
       {
-         data = new Data();
-         data.key = key;
-         value = defaultValue;
+         data = new SaveManager.Data<T>(key, defaultValue);
+      }
+
+      public override string ToString()
+      {
+         return data.ToString();
       }
    }
 }
